fa3c3cf90c17f5c0b81638538105dddb425dd0ef
AnhTT <kimcuongden1712@gmail.com>
2022-08-21 20:44:53 +0700

    update cmt

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require("fs");
-
+//Blocking, synchronous way
 const hello = "Hello Node";
 console.log(hello);
-
 const txtIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(txtIn);
 const txtOut = `This is what about th avocado ${txtIn} ${Date.now()}`;

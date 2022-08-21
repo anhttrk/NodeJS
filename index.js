989be1f2e2d115c9712f1139b05600998798d975
AnhTT <kimcuongden1712@gmail.com>
2022-08-21 20:42:10 +0700

    2-6~2-9

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const hello = "Hello Node";
+console.log(hello);
+
+const txtIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(txtIn);
+const txtOut = `This is what about th avocado ${txtIn} ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", txtOut);
+console.log("File written!");
+const txtOutput = fs.readFileSync("./txt/output.txt", "utf-8");
+console.log(txtOutput);
